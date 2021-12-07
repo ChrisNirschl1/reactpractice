540f00550aebe744c46e368f034ab28877caa327
Christian Nirschl <christiannirschl6@gmail.com>
2021-12-07 13:21:44 -0800

    resume

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Resume() {
     return (
         <div>
-            Resume time
+            <h1>Checkout my Resume!</h1>
+            <div>
+
+             <a href="https://docs.google.com/document/d/1UXGj1l9vpcjwNTW8LV647IRPwXKer0wCwdvWyCq8MUw/edit?usp=sharing">View my resume </a>
+            </div>
         </div>
     )
 }
